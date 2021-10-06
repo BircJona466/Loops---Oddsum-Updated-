330d2ea45a1d76b0ffad650f1e51308d3591e703
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-06 13:55:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops___Oddsum_Updated_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+
+            Console.WriteLine("Enter a NUMBER!!");
+            if (Int32.TryParse(Console.ReadLine(), out input))
+            {
+                for (int i = 1; i < input; i = i + 2)
+                {
+                    Console.WriteLine(Convert.ToString(i));
+                }
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("That's not a number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
